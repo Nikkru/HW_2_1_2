7dcb6c8b0d25744ae032ffc6ebc71b0f664e6d0f
Nikkru <56563372+Nikkru@users.noreply.github.com>
2020-05-19 20:49:03 +0300

    viewdidload set

--- HW_2_1_2/ViewController.swift
+++ HW_2_1_2/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var redTraficLightView: UIView!
     @IBOutlet weak var yellowTraficLightView: UIView!
     @IBOutlet weak var greenTraficLightView: UIView!
     
+    @IBOutlet weak var switchTraficLightButtonText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switchTraficLightButtonText.layer.cornerRadius = 10
+        
+        redTraficLightView.layer.cornerRadius = 60
+        yellowTraficLightView.layer.cornerRadius = 60
+        greenTraficLightView.layer.cornerRadius = 60
+        
+        redTraficLightView.layer.opacity = 0.2
+        yellowTraficLightView.layer.opacity = 0.2
+        greenTraficLightView.layer.opacity = 0.2
     }
-
+    
     @IBAction func switchTraficLightButton() {
+        
     }
     
 }
